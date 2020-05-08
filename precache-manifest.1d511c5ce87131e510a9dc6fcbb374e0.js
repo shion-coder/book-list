93000399bfb81fd856ca90542155da6f5c970bba
Shion <shionchill@Shions-MacBook-Pro.local>
2020-05-09 01:12:24 +0700

    Updates

---
+++ precache-manifest.1d511c5ce87131e510a9dc6fcbb374e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09e28c6873dc2ee1dc4f16fc37d4108e",
+    "url": "/book-list/index.html"
+  },
+  {
+    "revision": "14c65445b99b47178293",
+    "url": "/book-list/static/js/2.e138f7df.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/book-list/static/js/2.e138f7df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32b876e01c91a5d15881",
+    "url": "/book-list/static/js/main.04626c01.chunk.js"
+  },
+  {
+    "revision": "b74e72dd593f60d10ca9",
+    "url": "/book-list/static/js/runtime-main.0634ad0e.js"
+  }
+]);
